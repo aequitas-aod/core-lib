3584f02ebbd00ed8b0ea34b7debe5c965e2e1120
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-07-31 15:02:07 +0200

    feat: implement and test demographic parity

--- test/__init__.py
+++ test/__init__.py
@@ -1,9 +1,25 @@
-import unittest
-from aequitas import *
+import numpy as np
+import numpy.random as random
+import typing
 
 
-class TestMyClass(unittest.TestCase):
-    # test methods' names should begin with `test_`
-    def test_my_method(self):
-        x = "Hello World"
-        self.assertEqual("Hello World", x)
+SEED = 1
+random.seed(SEED)
+
+
+def uniform_binary_dataset(rows: int = 1000, columns: int = 2) -> np.array:
+    return random.uniform(0, 1, size=(rows, columns)).round().astype(int)
+
+
+def bernoulli(p: float, size: typing.Tuple[int, int] = (1,)) -> np.array:
+    assert 0 <= p <= 1, "p must be in [0, 1]"
+    return (random.uniform(0, 1, size=size) < p).astype(int)
+
+
+def skewed_binary_dataset(rows: int = 1000, columns: int = 2, p: float = 0.8) -> np.array:
+    xs = uniform_binary_dataset(rows, 1)
+    cols = []
+    for _ in range(columns - 1):
+        ys = np.array([bernoulli(p)[0] * x for x in xs])
+        cols.append(ys)
+    return np.concatenate([xs] + cols, axis=1)
