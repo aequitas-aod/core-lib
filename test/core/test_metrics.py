1b5abfb88eebb2d0fff1f87186b5e89b4dc2130c
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-12-04 11:08:24 +0100

    chore(test): use unittest assertions in test_metrics.py

--- test/core/test_metrics.py
+++ test/core/test_metrics.py
@@ -11,18 +11,23 @@ class TestDemographicParity(unittest.TestCase):
         self.fair_dataset = uniform_binary_dataset(rows=DATASET_SIZE)
         self.unfair_dataset = skewed_binary_dataset(rows=DATASET_SIZE, p=0.9)
 
+    def assertInRange(self, value, lower, upper):
+        self.assertGreaterEqual(value, lower)
+        self.assertLessEqual(value, upper)
+
     def test_parity_on_fair_binary_case(self):
         x = self.fair_dataset[:, 0]
         y = self.fair_dataset[:, 1]
         parities = discrete_demographic_parities(x, y, 1)
-        assert parities.shape == (1,)
-        assert 0.0 < parities[0] <= 0.005
+        self.assertEqual(parities.shape, (1,))
+        self.assertInRange(parities[0], 0.0, 0.005)
 
     def test_parity_on_unfair_binary_case(self):
         x = self.unfair_dataset[:, 0]
         y = self.unfair_dataset[:, 1]
         parities = discrete_demographic_parities(x, y, 1)
-        assert 0.4 < parities[0] <= 0.5
+        self.assertEqual(parities.shape, (1,))
+        self.assertInRange(parities[0], 0.4, 0.5)
 
 
 if __name__ == '__main__':
