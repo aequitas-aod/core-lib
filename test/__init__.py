9df8ac09639ab34d9add285b038fe23409d1f92b
Liam James <liamjames@lana.local>
2024-01-17 16:56:16 +0100

    test: biased and unbiased datasets with probabilities

--- test/__init__.py
+++ test/__init__.py
@@ -2,7 +2,6 @@ import numpy as np
 import numpy.random as random
 import typing
 
-
 SEED = 1
 random.seed(SEED)
 
@@ -13,18 +12,19 @@ def uniform_binary_dataset(rows: int = 1000, columns: int = 2) -> np.array:
 
 def uniform_binary_dataset_gt(rows: int = 1000, columns: int = 2) -> np.array:
     xs = uniform_binary_dataset(rows, 1)
-    labels = uniform_binary_dataset(rows, 1) 
+    labels = uniform_binary_dataset(rows, 1)
     noise = random.choice([0, 1], p=[0.8, 0.2], size=(rows, 1))
-    preds = abs(labels-noise)
+    preds = abs(labels - noise)
     if columns > 2:
         data = []
         for _ in range(columns - 2):
             data.append(uniform_binary_dataset(rows, 1))
         data = np.concatenate(data, axis=1)
-        return np.concatenate((data, xs, labels, preds), axis=1) 
+        return np.concatenate((data, xs, labels, preds), axis=1)
     else:
         return np.concatenate((xs, labels, preds), axis=1)
-    
+
+
 def skewed_binary_dataset_gt(rows: int = 1000, columns: int = 2, p: float = 0.8) -> np.array:
     xs = uniform_binary_dataset(rows, 1)
     preds = np.array([bernoulli(p)[0] * x for x in xs])
@@ -34,9 +34,10 @@ def skewed_binary_dataset_gt(rows: int = 1000, columns: int = 2, p: float = 0.8)
         for _ in range(columns - 2):
             data.append(uniform_binary_dataset(rows, 1))
         data = np.concatenate(data, axis=1)
-        return np.concatenate((data, xs, labels, preds), axis=1) 
+        return np.concatenate((data, xs, labels, preds), axis=1)
     else:
         return np.concatenate((xs, labels, preds), axis=1)
+
 
 def bernoulli(p: float, size: typing.Tuple[int, int] = (1,)) -> np.array:
     assert 0 <= p <= 1, "p must be in [0, 1]"
@@ -50,3 +51,22 @@ def skewed_binary_dataset(rows: int = 1000, columns: int = 2, p: float = 0.8) ->
         ys = np.array([bernoulli(p)[0] * x for x in xs])
         cols.append(ys)
     return np.concatenate([xs] + cols, axis=1)
+
+
+def uniform_binary_dataset_probs(rows: int = 1000, columns: int = 2) -> np.array:
+    probs = random.uniform(0, 1, (rows, 1))
+    xs = uniform_binary_dataset(rows, columns)
+    labels = uniform_binary_dataset(rows, 1)
+    return np.concatenate((xs, probs, labels), axis=1)
+
+
+def skewed_binary_dataset_probs(rows: int = 1000, columns: int = 2, p: float =
+                               0.8) -> np.array:
+    features = uniform_binary_dataset(rows, columns)
+    xs = uniform_binary_dataset(rows, 1)
+
+    probs = np.array([random.normal(0.3, 0.1, 1) if x == 0 else
+                      random.normal(0.7, 0.1, 1) for x in xs])
+    labels = (probs >= 0.5).astype(int)
+
+    return np.concatenate((features, xs, probs, labels), axis=1)
