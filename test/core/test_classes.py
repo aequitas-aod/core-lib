17c0552fedaf1994fdcf241dd373227c462f698b
Liam James <liamjames@lana.fritz.box>
2024-03-07 00:49:00 +0100

    test: test factory pattern

--- test/core/test_classes.py
+++ test/core/test_classes.py
@@ -1,16 +1,19 @@
 import unittest
 from test import binary_label_dataset
-from aequitas.datasets.my_structured_dataset import MyStructuredDataset
+from test import binary_label_dataset_with_scores
+
+from aequitas.datasets.create_dataset import CreateDataset
 from aequitas.imputation_strategies.mcmc_imputation_strategy import MCMCImputationStrategy
 
 
 class TestClasses(unittest.TestCase):
 
-    def test_strategy_pattern(self):
+    def test_factory_pattern(self):
         df = binary_label_dataset()
         strategy = MCMCImputationStrategy()
-        ds = MyStructuredDataset(imputation_strategy=strategy, df=df, label_names=['label'],
-                                 protected_attribute_names=['feat'])
+        cd = CreateDataset(dataset_type="binary label")
+        ds = cd.create_dataset(imputation_strategy=strategy, df=df, label_names=['label'],
+                                    protected_attribute_names=['feat'])
         self.assertTrue(ds is not None)
 
 
