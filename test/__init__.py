5c55ee932d5fbbbebf9f558b301fd8ec2f80ca9f
liamjames2311 <liam.james2@unibo.it>
2024-03-07 15:42:16 +0100

    test: instantiate metric class

--- test/__init__.py
+++ test/__init__.py
@@ -8,3 +8,11 @@ def binary_label_dataset(rows: int = 1000) -> pd.DataFrame:
     labels = random.uniform(0, 1, size=(rows, 1)).astype(int)
     data = np.concatenate([features] + [labels], axis=1)
     return pd.DataFrame(data, columns=['feat', 'label'])
+
+def binary_label_dataset_with_scores(rows: int = 1000) -> pd.DataFrame:
+    features = random.uniform(0, 1, size=(rows, 1))
+    scores = random.uniform(0, 1, size=(rows, 1))
+    labels = (scores > 0.5).astype(int)
+    data = np.concatenate([features] + [scores] + [labels], axis=1)
+    return pd.DataFrame(data, columns=['feat', 'scores', 'label'])
+
--- test/core/test_classes.py
+++ test/core/test_classes.py
@@ -4,6 +4,7 @@ from test import binary_label_dataset_with_scores
 
 from aequitas.core.datasets.create_dataset import CreateDataset
 from aequitas.core.imputation_strategies.mcmc_imputation_strategy import MCMCImputationStrategy
+from aequitas.core.metrics.binary_label_dataset_scores_metric import BinaryLabelDatasetScoresMetric
 
 
 class TestClasses(unittest.TestCase):
@@ -24,6 +25,16 @@ class TestClasses(unittest.TestCase):
                                protected_attribute_names=['feat'], scores_names=["scores"])
         self.assertTrue(ds is not None)
 
+    def test_metric(self):
+        df = binary_label_dataset_with_scores()
+        strategy = MCMCImputationStrategy()
+        cd = CreateDataset(dataset_type="binary label")
+        ds = cd.create_dataset(imputation_strategy=strategy, df=df, label_names=['label'],
+                               protected_attribute_names=['feat'], scores_names=["scores"])
+        bldsm = BinaryLabelDatasetScoresMetric(ds)
+        x = bldsm.scores_metric()
+        self.assertTrue(ds is not None)
+        self.assertTrue(x is not None)
 
 if __name__ == '__main__':
     unittest.main()
