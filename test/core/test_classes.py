3165cd4f09e12af5d82b431c65a18605dcc5f1cb
liamjames2311 <liam.james2@unibo.it>
2024-03-07 14:06:11 +0100

    chore: preserve "core" structure

--- test/core/test_classes.py
+++ test/core/test_classes.py
@@ -2,18 +2,26 @@ import unittest
 from test import binary_label_dataset
 from test import binary_label_dataset_with_scores
 
-from aequitas.datasets.create_dataset import CreateDataset
-from aequitas.imputation_strategies.mcmc_imputation_strategy import MCMCImputationStrategy
+from aequitas.core.datasets.create_dataset import CreateDataset
+from aequitas.core.imputation_strategies.mcmc_imputation_strategy import MCMCImputationStrategy
 
 
 class TestClasses(unittest.TestCase):
 
-    def test_factory_pattern(self):
+    def test_factory_pattern_bld(self):
         df = binary_label_dataset()
         strategy = MCMCImputationStrategy()
         cd = CreateDataset(dataset_type="binary label")
         ds = cd.create_dataset(imputation_strategy=strategy, df=df, label_names=['label'],
-                                    protected_attribute_names=['feat'])
+                               protected_attribute_names=['feat'])
+        self.assertTrue(ds is not None)
+
+    def test_factory_pattern_bld_scores(self):
+        df = binary_label_dataset_with_scores()
+        strategy = MCMCImputationStrategy()
+        cd = CreateDataset(dataset_type="binary label")
+        ds = cd.create_dataset(imputation_strategy=strategy, df=df, label_names=['label'],
+                               protected_attribute_names=['feat'], scores_names=["scores"])
         self.assertTrue(ds is not None)
 
 
