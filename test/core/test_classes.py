0bad1f14d5d73e6ee94272d9f33305efdab9ebc7
liamjames2311 <liam.james2@unibo.it>
2024-05-13 14:26:06 +0200

    test: refactoring + tests

--- test/core/test_classes.py
+++ test/core/test_classes.py
@@ -1,9 +1,18 @@
 import unittest
 import pandas as pd
-from test import generate_binary_label_dataframe
-from test import generate_binary_label_dataframe_with_scores
+from test import (
+    generate_binary_label_dataframe,
+    generate_binary_label_dataframe_with_scores,
+    generate_multi_label_dataframe,
+    generate_multi_label_dataframe_with_scores
+)
 
-from aequitas.core.datasets import create_dataset, BinaryLabelDataset
+from aequitas.core.datasets import (
+    create_dataset,
+    BinaryLabelDataset,
+    MulticlassLabelDataset
+)
+
 from aequitas.core.imputation_strategies import MCMCImputationStrategy
 from aequitas.core.metrics import BinaryLabelDatasetScoresMetric
 
@@ -15,58 +24,153 @@ class TestBinaryLabelDataset(unittest.TestCase):
                             # parameters of aequitas.BinaryLabelDataset init
                             unprivileged_groups=[{'prot_attr': 0}],
                             privileged_groups=[{'prot_attr': 1}],
-                            # parameters of aif360.StructuredDataset init
-                            df=generate_binary_label_dataframe(),
-                            label_names=['label'],
-                            protected_attribute_names=['prot_attr'],
                             # parameters of aequitas.StructuredDataset init
                             imputation_strategy=MCMCImputationStrategy(),
                             # parameters of aif360.BinaryLabelDataset init
                             favorable_label=1,
-                            unfavorable_label=0
+                            unfavorable_label=0,
+                            # parameters of aif360.StructuredDataset init
+                            df=generate_binary_label_dataframe(),
+                            label_names=['label'],
+                            protected_attribute_names=['prot_attr']
                             )
+        mro = False
+        if mro:
+            print(f"{ds.__class__.__mro__} MRO (aequitas): {ds.__class__.__mro__}")
         self.assertIsInstance(ds, BinaryLabelDataset)
         self.assertIsNotNone(ds)
 
     def test_dataset_creation_with_scores_via_factory(self):
         ds = create_dataset("binary label",
-                            # parameters of aequitas.BinaryLabelDataset
+                            # parameters of aequitas.BinaryLabelDataset init
                             unprivileged_groups=[{'prot_attr': 0}],
                             privileged_groups=[{'prot_attr': 1}],
-                            # parameters of aif360.StructuredDataset init
-                            df=generate_binary_label_dataframe_with_scores(),
-                            label_names=['label'],
-                            protected_attribute_names=['prot_attr'],
-                            scores_names=['scores'],
                             # parameters of aequitas.StructuredDataset init
                             imputation_strategy=MCMCImputationStrategy(),
                             # parameters of aif360.BinaryLabelDataset init
                             favorable_label=1,
-                            unfavorable_label=0)
+                            unfavorable_label=0,
+                            # parameters of aif360.StructuredDataset init
+                            df=generate_binary_label_dataframe_with_scores(),
+                            label_names=['label'],
+                            protected_attribute_names=['prot_attr'],
+                            scores_names="score"
+                            )
+        mro = False
+        if mro:
+            print(f"{ds.__class__.__mro__} MRO (aequitas): {ds.__class__.__mro__}")
         self.assertIsInstance(ds, BinaryLabelDataset)
         self.assertIsNotNone(ds)
 
     def test_metrics_on_dataset(self):
         ds = create_dataset("binary label",
-                            # parameters of aequitas.BinaryLabelDataset
+                            # parameters of aequitas.BinaryLabelDataset init
                             unprivileged_groups=[{'prot_attr': 0}],
                             privileged_groups=[{'prot_attr': 1}],
-                            # parameters of aif360.StructuredDataset init
-                            df=generate_binary_label_dataframe_with_scores(),
-                            label_names=['label'],
-                            protected_attribute_names=['prot_attr'],
-                            scores_names=['score'],
                             # parameters of aequitas.StructuredDataset init
                             imputation_strategy=MCMCImputationStrategy(),
                             # parameters of aif360.BinaryLabelDataset init
                             favorable_label=1,
-                            unfavorable_label=0)
+                            unfavorable_label=0,
+                            # parameters of aif360.StructuredDataset init
+                            df=generate_binary_label_dataframe_with_scores(),
+                            label_names=['label'],
+                            protected_attribute_names=['prot_attr'],
+                            scores_names="score"
+                            )
+        mro = False
+        if mro:
+            print(f"{ds.__class__.__mro__} MRO (aequitas): {ds.__class__.__mro__}")
         self.assertIsInstance(ds.scores_metrics, BinaryLabelDatasetScoresMetric)
         self.assertIsNotNone(ds)
+
         score = ds.metrics.disparate_impact()
+        print(f"Disparate impact: {score}")
         self.assertIsNotNone(score)
+
+        score = ds.metrics.statistical_parity_difference()
+        print(f"Statistical Parity: {score}")
+        self.assertIsNotNone(score)
+
         score = ds.scores_metrics.new_fancy_metric()
         self.assertIsNotNone(score)
+
+
+class TestMulticlassLabelDataset(unittest.TestCase):
+    def test_dataset_creation_via_factory(self):
+        ds = create_dataset("multi class",
+                            # parameters of aequitas.MulticlassLabelDataset init
+                            unprivileged_groups=[{'prot_attr': 0}],
+                            privileged_groups=[{'prot_attr': 1}],
+                            # parameters of aequitas.StructuredDataset init
+                            imputation_strategy=MCMCImputationStrategy(),
+                            # parameters of aif360.MulticlassLabelDataset init
+                            favorable_label=[0, 1., 2.],
+                            unfavorable_label=[3., 4.],
+                            # parameters of aif360.StructuredDataset init
+                            df=generate_multi_label_dataframe(),
+                            label_names=['label'],
+                            protected_attribute_names=['prot_attr']
+                            )
+        mro = False
+        if mro:
+            print(f"{ds.__class__.__mro__} MRO (aequitas): {ds.__class__.__mro__}")
+        self.assertIsInstance(ds, MulticlassLabelDataset)
+        self.assertIsNotNone(ds)
+
+    def test_dataset_creation_with_scores_via_factory(self):
+        ds = create_dataset("multi class",
+                            # parameters of aequitas.MulticlassLabelDataset init
+                            unprivileged_groups=[{'prot_attr': 0}],
+                            privileged_groups=[{'prot_attr': 1}],
+                            # parameters of aequitas.StructuredDataset init
+                            imputation_strategy=MCMCImputationStrategy(),
+                            # parameters of aif360.MulticlassLabelDataset init
+                            favorable_label=[0, 1., 2.],
+                            unfavorable_label=[3., 4.],
+                            # parameters of aif360.StructuredDataset init
+                            df=generate_multi_label_dataframe_with_scores(),
+                            label_names=['label'],
+                            protected_attribute_names=['prot_attr'],
+                            scores_names="score"
+                            )
+        mro = False
+        if mro:
+            print(f"{ds.__class__.__mro__} MRO (aequitas): {ds.__class__.__mro__}")
+        self.assertIsInstance(ds, MulticlassLabelDataset)
+        self.assertIsNotNone(ds)
+
+    def test_metrics_on_dataset(self):
+        ds = create_dataset("multi class",
+                            # parameters of aequitas.MulticlassLabelDataset init
+                            unprivileged_groups=[{'prot_attr': 0}],
+                            privileged_groups=[{'prot_attr': 1}],
+                            # parameters of aequitas.StructuredDataset init
+                            imputation_strategy=MCMCImputationStrategy(),
+                            # parameters of aif360.MulticlassLabelDataset init
+                            favorable_label=[0, 1., 2.],
+                            unfavorable_label=[3., 4.],
+                            # parameters of aif360.StructuredDataset init
+                            df=generate_multi_label_dataframe_with_scores(),
+                            label_names=['label'],
+                            protected_attribute_names=['prot_attr'],
+                            scores_names="score"
+                            )
+        mro = False
+        if mro:
+            print(f"{ds.__class__.__mro__} MRO (aequitas): {ds.__class__.__mro__}")
+
+        self.assertIsInstance(ds, MulticlassLabelDataset)
+        self.assertIsNotNone(ds)
+
+        score = ds.metrics.disparate_impact()
+        print(f"Disparate impact: {score}")
+        self.assertIsNotNone(score)
+
+        score = ds.scores_metrics.new_fancy_metric()
+        self.assertIsNotNone(score)
+
+
 
 
 if __name__ == '__main__':
