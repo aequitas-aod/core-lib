e38334c0c394d5407dc10f2c77c64474d313f374
liamjames2311 <liam.james2@unibo.it>
2024-05-13 14:27:38 +0200

    test: init multi label dataset

--- test/__init__.py
+++ test/__init__.py
@@ -10,6 +10,7 @@ def generate_binary_label_dataframe(rows: int = 1000) -> pd.DataFrame:
     data = np.concatenate([features] + [prot_attr] + [labels], axis=1)
     return pd.DataFrame(data, columns=['feat', 'prot_attr', 'label'])
 
+
 def generate_binary_label_dataframe_with_scores(rows: int = 1000) -> pd.DataFrame:
     features = random.uniform(0, 1, size=(rows, 1))
     prot_attr = np.random.randint(2, size=(rows, 1))
@@ -18,3 +19,20 @@ def generate_binary_label_dataframe_with_scores(rows: int = 1000) -> pd.DataFram
     data = np.concatenate([features] + [prot_attr] + [scores] + [labels], axis=1)
     return pd.DataFrame(data, columns=['feat', 'prot_attr', 'score', 'label'])
 
+
+def generate_multi_label_dataframe(rows: int = 1000) -> pd.DataFrame:
+    features = random.uniform(0, 1, size=(rows, 1))
+    prot_attr = np.random.randint(2, size=(rows, 1))
+    labels = np.random.randint(5, size=(rows, 1))
+    data = np.concatenate([features] + [prot_attr] + [labels], axis=1)
+    res = pd.DataFrame(data, columns=['feat', 'prot_attr', 'label'])
+    return res
+
+
+def generate_multi_label_dataframe_with_scores(rows: int = 1000) -> pd.DataFrame:
+    features = random.uniform(0, 1, size=(rows, 1))
+    prot_attr = np.random.randint(2, size=(rows, 1))
+    scores = random.uniform(0, 1, size=(rows, 1))
+    labels = np.random.randint(5, size=(rows, 1))
+    data = np.concatenate([features] + [prot_attr] + [scores] + [labels], axis=1)
+    return pd.DataFrame(data, columns=['feat', 'prot_attr', 'score', 'label'])
