e297ebc001e462a48b70663357b2c5b64586ad61
liamjames2311 <liam.james2@unibo.it>
2024-05-16 17:19:08 +0200

    test: adversarial debiasing

--- test/core/test_classes.py
+++ test/core/test_classes.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, MaxAbsScaler
 from sklearn.linear_model import LogisticRegression
 
 from test import (
@@ -28,6 +28,11 @@ from aequitas.core.algorithms import create_algorithm
 from aif360.algorithms.preprocessing.optim_preproc_helpers.data_preproc_functions import load_preproc_data_adult
 
 from aequitas.core.algorithms.preprocessing.optim_preproc_helpers import load_preproc_data_adult_aeq
+from aequitas.core.datasets.zoo import adult
+
+import tensorflow.compat.v1 as tf
+tf.disable_eager_execution()
+
 
 class TestBinaryLabelDataset(unittest.TestCase):
 
@@ -333,13 +338,14 @@ class TestMitigationAlgorithms(unittest.TestCase):
 
     def test_disparate_impact_remover_on_adult_dataset(self):
         protected = "sex"
-        ds = create_dataset("adult",
-                            unprivileged_groups=[{protected: 0}],
-                            privileged_groups=[{protected: 1}],
-                            protected_attribute_names=[protected],
-                            privileged_classes=[['Male']],
-                            categorical_features=[],
-                            features_to_keep=['age', 'education-num', 'capital-gain', 'capital-loss', 'hours-per-week'])
+
+        ds = adult("adult",
+                   unprivileged_groups=[{protected: 0}],
+                   privileged_groups=[{protected: 1}],
+                   protected_attribute_names=[protected],
+                   privileged_classes=[['Male']],
+                   categorical_features=[],
+                   features_to_keep=['age', 'education-num', 'capital-gain', 'capital-loss', 'hours-per-week'])
 
         scaler = MinMaxScaler(copy=False)
 
@@ -352,7 +358,8 @@ class TestMitigationAlgorithms(unittest.TestCase):
 
         di_remover = create_algorithm(algorithm_type="dir", sensitive_attribute=protected)
 
-        print(f"disparate impact before mitigation: {test.metrics.disparate_impact()}")
+        di_before = test.metrics.disparate_impact()
+        self.assertLess(di_before, 0.8, msg="DI is expected to be below 0.8 before mitigation")
 
         train_repd = di_remover.fit_transform(train)
         test_repd = di_remover.fit_transform(test)
@@ -367,7 +374,8 @@ class TestMitigationAlgorithms(unittest.TestCase):
         test_repd_pred = test_repd.copy()
         test_repd_pred.labels = lmod.predict(X_te)
 
-        print(f"disparate impact after mitigation: {test_repd_pred.metrics.disparate_impact()}")
+        di_after = test_repd_pred.metrics.disparate_impact()
+        self.assertGreaterEqual(di_after, 0.8, msg="DI is expected to be above 0.8 after mitigation")
 
     def test_reweighing_on_adult_dataset(self):
         protected = "sex"
@@ -383,8 +391,69 @@ class TestMitigationAlgorithms(unittest.TestCase):
             f"Difference in mean outcomes between unprivileged and privileged groups after reweighing: {repaired_ds.metrics.mean_difference()}")
 
     def test_adversarial_debiasing_on_adult_dataset(self):
-        ds = load_preproc_data_adult()
+        attr = "sex"
+        u = [{attr: 0}]
+        p = [{attr: 1}]
+        ds = load_preproc_data_adult_aeq(unprivileged_groups=u,
+                                         privileged_groups=p)
 
+        train, test = ds.split([0.7], shuffle=True)
+        metric_orig_train = train.metrics.mean_difference()
+        metric_orig_test = test.metrics.mean_difference()
+
+        print("Metrics from original data")
+        print(f"Train set: Difference in mean outcomes between unprivileged and privileged groups: {metric_orig_train}")
+        print(f"Train set: Difference in mean outcomes between unprivileged and privileged groups: {metric_orig_test}")
+        print("#" * 10 + "\n")
+
+        min_max_scaler = MaxAbsScaler()
+        train.features = min_max_scaler.fit_transform(train.features)
+        test.features = min_max_scaler.transform(test.features)
+
+        sess = tf.Session()
+        plain_model = create_algorithm(algorithm_type="adversarial debiasing",
+                                       privileged_groups=p,
+                                       unprivileged_groups=u,
+                                       scope_name='plain_classifier',
+                                       debias=False,
+                                       sess=sess)
+
+        plain_model.fit(train)
+
+        nondebiasing_train = plain_model.predict(train)
+        nondebiasing_test = plain_model.predict(test)
+
+        metric_nondebiasing_train = nondebiasing_train.metrics.mean_difference()
+        metric_nondebiasing_test = nondebiasing_test.metrics.mean_difference()
+
+        print("Metrics from plain model")
+        print(f"Train set: Difference in mean outcomes between unprivileged and privileged groups: {metric_nondebiasing_train}")
+        print(f"Train set: Difference in mean outcomes between unprivileged and privileged groups: {metric_nondebiasing_test}")
+        print("#" * 10 + "\n")
+
+
+        sess.close()
+        tf.reset_default_graph()
+        sess = tf.Session()
+
+        debiased_model = create_algorithm(algorithm_type="adversarial debiasing",
+                                          privileged_groups=p,
+                                          unprivileged_groups=u,
+                                          scope_name='debiased_classifier',
+                                          debias=True,
+                                          sess=sess)
+        debiased_model.fit(train)
+
+        debiasing_train = debiased_model.predict(train)
+        debiasing_test = debiased_model.predict(test)
+
+        metric_debiasing_train = debiasing_train.metrics.mean_difference()
+        metric_debiasing_test = debiasing_test.metrics.mean_difference()
+
+        print("Metrics from debiased model")
+        print("Train set: Difference in mean outcomes between unprivileged and privileged groups = %f" % metric_debiasing_train)
+        print("Train set: Difference in mean outcomes between unprivileged and privileged groups = %f" % metric_debiasing_test)
+        print("#" * 10 + "\n")
 
 
 if __name__ == '__main__':
