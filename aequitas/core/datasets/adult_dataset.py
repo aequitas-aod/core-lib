766afd78d88c1610913d59d39f17d47ee0c173f6
liamjames2311 <liam.james2@unibo.it>
2024-05-16 17:17:52 +0200

    feat: add score metrics for adult dataset

--- aequitas/core/datasets/adult_dataset.py
+++ aequitas/core/datasets/adult_dataset.py
@@ -1,15 +1,21 @@
 import aif360.datasets as datasets
 from aif360.metrics import BinaryLabelDatasetMetric
+from aif360.algorithms.preprocessing.optim_preproc_helpers.data_preproc_functions import load_preproc_data_adult
+from aequitas.core.metrics import BinaryLabelDatasetScoresMetric
 
 
 class AdultDataset(datasets.AdultDataset):
-    def __init__(self, **kwargs):
+    def __init__(self, unprivileged_groups, privileged_groups, **kwargs):
+        self.unprivileged_groups = unprivileged_groups
+        self.privileged_groups = privileged_groups
         super(AdultDataset, self).__init__(**kwargs)
-
-        self.unprivileged_groups = [{attr_name: 0} for attr_name in self.protected_attribute_names]
-        self.privileged_groups = [{attr_name: 1} for attr_name in self.protected_attribute_names]
 
     @property
     def metrics(self):
         return BinaryLabelDatasetMetric(dataset=self, unprivileged_groups=self.unprivileged_groups,
                                         privileged_groups=self.privileged_groups)
+
+    @property
+    def score_metrics(self):
+        return BinaryLabelDatasetScoresMetric(dataset=self, unprivileged_groups=self.unprivileged_groups,
+                                              privileged_groups=self.privileged_groups)
