1c8acaea5e86f6d343666929b92fd6bec14a7843
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2024-05-20 15:36:23 +0200

    test: add tests for copy and split

--- test/core/test_dataset_decorators.py
+++ test/core/test_dataset_decorators.py
@@ -20,3 +20,17 @@ class TestBinaryLabelDataset(unittest.TestCase):
 
     def test_metrics(self):
         self.assertIsInstance(self._dataset.metrics, BinaryLabelDatasetMetric)
+
+    def test_copy_preserves_type(self):
+        copied = self._dataset.copy()
+        self.assertIsInstance(copied, DatasetWithBinaryLabelMetrics)
+        self.assertIsInstance(copied._delegate, BinaryLabelDataset)
+        self.assertIsInstance(copied.metrics, BinaryLabelDatasetMetric)
+
+    def test_split_preserves_type(self):
+        splitted = self._dataset.split([0.5])
+        for split in splitted:
+            self.assertIsInstance(split, DatasetWithBinaryLabelMetrics)
+            self.assertIsInstance(split._delegate, BinaryLabelDataset)
+            self.assertIsInstance(split.metrics, BinaryLabelDatasetMetric)
+    
